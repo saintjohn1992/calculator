5f3ddaf053f95bc0fb6acccf6fe2a7580ff14508
Shane St John <saintjohnshane@outlook.com>
2022-02-24 14:33:00 +0100

    final touches to html and setting up javascript variables

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-equals]')
+const previousOperandTextElement = document.querySelector('[data-equals]')
+const currentOperandTextElement = document.querySelector('[data-equals]')
